cc58e15e5c7065da8c43172062d33ad2e6ab0b2e
unnamedninja <zyvchuk@gmail.com>
2020-03-24 15:57:09 +0200

    problems with main nav

--- js/myscript.js
+++ js/myscript.js
@@ -1,16 +1,24 @@
-//---Кнопка "ВВЕРХ"---
+
 //Get the button
 var mybutton = document.getElementById("myBtn");
+var prevScrollpos = window.pageYOffset;
+document.getElementById("mainNav").style.top = "0";
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
+window.onscroll = function() {
   if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
+
+
+  var currentScrollPos = window.pageYOffset;
+  document.getElementById("mainNav").style.top = "0";
+  if (prevScrollpos < currentScrollPos) {
+    document.getElementById("mainNav").style.top = "-80px";
+  }
+  prevScrollpos = currentScrollPos;
+
 }
 
 // When the user clicks on the button, scroll to the top of the document
